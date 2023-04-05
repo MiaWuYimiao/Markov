b6e9e2028b2ddc6f01d842f7387efd3270749483
Yimiao Wu <wuyimiao51s@icloud.com>
2023-04-04 23:41:56 -0400

    Edit test

--- markov.test.js
+++ markov.test.js
@@ -2,23 +2,23 @@ const { MarkovMachine } = require("./markov");
 const fs = require('fs')
 
 describe("Test class MarkovMachine", function () {
-    let text;
-    beforeEach(function () {
-        fs.readFile("eggs.txt", 'utf8', function(err, data) {
-            if(err) {
-                console.error(err);
-                process.exit(1);
-            }
-            console.log(data)
-            text = data;
-        })
-    })
+    // let text;
+    // beforeEach(function () {
+    //     fs.readFile("eggs.txt", 'utf8', function(err, data) {
+    //         if(err) {
+    //             console.error(err);
+    //             process.exit(1);
+    //         }
+    //         console.log(data)
+    //         text = data;
+    //     })
+    // })
 
-    test("makeChains", function () {
-        let mm = new MarkovMachine(text);
-        let chains = mm.chains;
-        expect(chains).toEqual(expect.any(Map));
-    });
+    // test("makeChains", function () {
+    //     let mm = new MarkovMachine(text);
+    //     let chains = mm.chains;
+    //     expect(chains).toEqual(expect.any(Map));
+    // });
 
     test('makes chains', function () {
         let mm = new MarkovMachine("aa bb cc aa BB aa BB");
@@ -28,7 +28,34 @@ describe("Test class MarkovMachine", function () {
           ["bb", ["cc"]],
           ["cc", ["aa"]],
           ["BB", ["aa", null]]]));
-        expect(chains).toEqual(expect.any(Map));
-      });
+        expect(mm.chains).toEqual(expect.any(Map));
+    });
+
+    test('generates semi-predictable text', function () {
+        let mm = new MarkovMachine("a b c");
+        let text = mm.makeText();
+        expect(["a b c", "b c", "c"]).toContain(text);
+    });
+
+    test('generate valid text', function () {
+        let mm = new MarkovMachine("the cat in the hat");
+        let text = mm.makeText();
+        expect(text.endsWith('hat')).toBe(true);
+
+        let bigrams = ["the cat", "cat in", "in the", "the hat", "hat "];
+        let textWords = text.split(/[ \r\n]+/);
+        for(let i = 0; i < textWords.length-1; i++) {
+            expect(bigrams).toContain(textWords[i] + " " + textWords[i+1]);
+        }
+    });
+
+    test('cuts off at length', function() {
+        let bigrams = ["the cat", "cat in", "in the", "the hat", "hat "];
+        let mm = new MarkovMachine("the cat in the hat");
+        let text = mm.makeText(2);
+        let textWords = text.split(/[ \r\n]+/);
+
+        expect([1, 2]).toContain(textWords.length);
+    });
 
 })
